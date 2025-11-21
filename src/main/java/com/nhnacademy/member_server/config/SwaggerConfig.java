55fe452f9cd3925e1a91aa0a5b98a2b7fa283364
songdohyeok <songdh801@naver.com>
2025-11-21 18:18:44 +0900

    ✨ feat: 포인트 API 스웨거 적용 [#2]

---
+++ src/main/java/com/nhnacademy/member_server/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.member_server.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Payment Server API")
+                        .description("결제 시스템 API 명세서입니다.")
+                        .version("1.0.0"));
+    }
+}
