f37e7fa8389137a124024e93905c969b42d86a65
songdohyeok <songdh801@naver.com>
2025-11-24 15:10:04 +0900

    ✨ feat: 래빗앰큐, 포인트 적립 임시 구현

--- src/main/java/com/nhnacademy/member_server/service/PointService.java
+++ src/main/java/com/nhnacademy/member_server/service/PointService.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.member_server.service;
 
+import com.nhnacademy.member_server.dto.PointEarnRequest;
 import com.nhnacademy.member_server.dto.PointTransactionRequest;
 import com.nhnacademy.member_server.entity.Member;
+import com.nhnacademy.member_server.entity.PointEventType;
 import com.nhnacademy.member_server.entity.PointHistory;
 import com.nhnacademy.member_server.repository.MemberRepository;
 import com.nhnacademy.member_server.repository.PointHistoryRepository;
@@ -17,8 +19,22 @@ public class PointService {
     private final PointHistoryRepository pointHistoryRepository;
 
 
-    public void earnPoint(PointTransactionRequest requestDto){
+    // 포인트 적립
+    public Long earnPoint(PointEarnRequest requestDto){
         Member member = memberRepository.findById(requestDto.getMemberId()).orElseThrow(() -> new RuntimeException("존재하지 않는 유저 입니다."));
+        long pointToEarn = 0;
+        String description = "";
+
+        // 상품 구매 시 등급별 비율 적용 적립
+        if(requestDto.getEventType() == PointEventType.ORDER){
+
+        }
+
+
+        // 이벤트 (리뷰, 회원가입)
+
+
+        return 0L;
     }
 
 
