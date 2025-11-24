f920367aa16efe91fd01e52b20d8da0d9d5baf92
songdohyeok <songdh801@naver.com>
2025-11-24 15:10:39 +0900

    ♻️ refactor: 주석 수정

--- src/main/java/com/nhnacademy/member_server/dto/PointTransactionRequest.java
+++ src/main/java/com/nhnacademy/member_server/dto/PointTransactionRequest.java
@@ -9,10 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PointTransactionRequest {
-    @Schema(description = "포인트 적립/사용/환불 될 유저 아이디", example = "1")
+    @Schema(description = "포인트 사용/환불 될 유저 아이디", example = "1")
     private Long memberId;
-    @Schema(description = "포인트 적립/사용/환불 액수", example = "100")
+    @Schema(description = "포인트 사용/환불 액수", example = "100")
     private Long amount;
-    @Schema(description = "포인트 적립/사용/환불 사유", example = "도서 구매: 배송 완료 포인트 적립")
+    @Schema(description = "포인트 사용/환불 사유", example = "도서 구매: 배송 완료 포인트 적립")
     private String description;
 }
