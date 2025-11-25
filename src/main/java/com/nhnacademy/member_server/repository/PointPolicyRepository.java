f7f40ab966b0303dd696e64d643508912a1ab722
songdohyeok <songdh801@naver.com>
2025-11-25 15:54:08 +0900

    ✨ feat: point 기본 구조 구현2 [#1]

---
+++ src/main/java/com/nhnacademy/member_server/repository/PointPolicyRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.member_server.repository;
+
+import com.nhnacademy.member_server.entity.PointPolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointPolicyRepository extends JpaRepository<PointPolicy, Long> {
+    PointPolicy findTopByOrderByUpdatedAtDesc(); // 수정일 기준 내림차순 정렬해서 맨위 1개 가져오기
+}
