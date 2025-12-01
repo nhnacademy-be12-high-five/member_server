e07be774d0d44416f0bda88128278a80366c0806
xxlsheep <eseuyun7@gmail.com>
2025-12-01 14:41:29 +0900

    refactor: 로그인 시간 DB 미반영 문제 수정 [#46]

--- src/main/java/com/nhnacademy/member_server/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/member_server/repository/MemberRepository.java
@@ -4,6 +4,9 @@ import com.nhnacademy.member_server.entity.Member;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
 import java.util.Optional;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.QueryHints;
@@ -12,4 +15,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
     Optional<Member> readById(Long id);
+
+    boolean existsByEmail(@Email String email);
+
+    boolean existsByPhone(@NotBlank String phone);
 }
