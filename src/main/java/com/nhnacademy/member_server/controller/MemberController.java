0a1dbb9cfcbcb6cfd01da85dd207733fb63050e0
xxlsheep <eseuyun7@gmail.com>
2025-12-01 15:55:49 +0900

    refactor: import 수정

--- src/main/java/com/nhnacademy/member_server/controller/MemberController.java
+++ src/main/java/com/nhnacademy/member_server/controller/MemberController.java
@@ -12,11 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/members")
