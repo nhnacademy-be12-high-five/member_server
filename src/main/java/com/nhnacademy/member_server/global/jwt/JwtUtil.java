959cb405edf06a403bc537a6c6a861fec83c996a
yunaa00 <0305lily@naver.com>
2025-12-23 15:24:14 +0900

    코드 추가
     .claim("memberId", memberId) 코드 추가

--- src/main/java/com/nhnacademy/member_server/global/jwt/JwtUtil.java
+++ src/main/java/com/nhnacademy/member_server/global/jwt/JwtUtil.java
@@ -38,6 +38,7 @@ public class JwtUtil {
 
         return Jwts.builder()
                 .setSubject(String.valueOf(memberId))
+                .claim("memberId", memberId)
                 .claim("role", role.name())
                 .setIssuedAt(now)
                 .setExpiration(expirationDate)
@@ -61,6 +62,7 @@ public class JwtUtil {
 
         return Jwts.builder()
                 .setSubject(String.valueOf(memberId))
+                .claim("memberId", memberId)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .signWith(secretKey, SignatureAlgorithm.HS256)
