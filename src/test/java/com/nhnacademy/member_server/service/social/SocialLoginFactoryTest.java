7492e28517335564645d9165dcb8b784c9fd6fea
xxlsheep <eseuyun7@gmail.com>
2025-12-29 09:35:43 +0900

    feat: 테스트 코드 추가 [#104]

---
+++ src/test/java/com/nhnacademy/member_server/service/social/SocialLoginFactoryTest.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.member_server.service.social;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class SocialLoginFactoryTest {
+
+    @Test
+    void getStrategy_Success() {
+        SocialLoginStrategy mockStrategy = mock(SocialLoginStrategy.class);
+        given(mockStrategy.getProviderName()).willReturn("PAYCO");
+
+        SocialLoginFactory factory = new SocialLoginFactory(List.of(mockStrategy));
+
+        SocialLoginStrategy result = factory.getStrategy("payco");
+
+        assertThat(result).isEqualTo(mockStrategy);
+    }
+
+    @Test
+    void getStrategy_Fail_NotSupported() {
+        SocialLoginFactory factory = new SocialLoginFactory(Collections.emptyList());
+
+        assertThatThrownBy(() -> factory.getStrategy("kakao"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("지원하지 않는 소셜 로그인입니다");
+    }
+}
